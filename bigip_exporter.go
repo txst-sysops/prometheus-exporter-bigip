f8abd91d5279db9fcea6169b80a4ee443ffdcc21
Tony Malkowski <tony@txstate.edu>
2024-12-11 21:09:17 -0600

    repo name

--- bigip_exporter.go
+++ bigip_exporter.go
@@ -5,8 +5,8 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/ExpressenAB/bigip_exporter/collector"
-	"github.com/ExpressenAB/bigip_exporter/config"
+	"github.com/txst-sysops/prometheus-exporter-bigip/collector"
+	"github.com/txst-sysops/prometheus-exporter-bigip/config"
 	"github.com/juju/loggo"
 	"github.com/pr8kerl/f5er/f5"
 	"github.com/prometheus/client_golang/prometheus"
