25cf3704778f9504eb5d390b510ec884bc61d75f
Tony Malkowski <tony@txstate.edu>
2024-12-11 20:21:12 -0600

    build.sh: strip out lin/win, focus on container

--- build.sh
+++ build.sh
@@ -1,85 +1,50 @@
-build() {
-  echo "Building"
-  build_windows
-  build_linux
-  build_docker $1
+#!/bin/bash
+
+DOCKER=$( ( which podman || which docker ) | head -1 )
+if [[ -z "$DOCKER" || ! -e "$DOCKER" ]]; then
+	echo "Cannot find any podman or docker executable in PATH" >&2
+	exit 1
+fi
+
+cd "$( dirname "$0" )"
+
+PROJECT_NAME=$( cat build.json | jq -r .project_name )
+
+fmt(){
+	find . -not -path "./vendor/*" -name "*.go" -exec go fmt {} \;
 }
 
-package() {
-  echo "Packaging"
-  package_windows $1
-  package_linux $1
+preflight(){
+	if [[ $( git status --porcelain=1 | wc -l ) -gt 0 ]]; then
+		echo "You have unstaged modifications in your repo." >&2
+		echo "Please commit and tag the new version before running build script." >&2
+		exit 1
+	fi
 }
 
-clean() {
-  echo "Cleaning"
-  clean_windows
-  clean_linux
+build(){
+	version=$( git tag --list --sort=version:refname | tail -1 )
+	echo "Initiating build process for version $version" >&2
+
+	tmp=$(mktemp)
+	"$DOCKER" build . -t "$id" | tee /dev/stderr > $tmp
+	if [[ $? != 0 ]]; then
+		echo "Docker build failed"
+		exit 1
+	fi
+	id=$( cat "$tmp" | awk 'END {print $3}' )
+	rm -f "$tmp"
+
+	echo $id
+	docker tag $id expressenab/bigip_exporter:$version
+	docker tag $id expressenab/bigip_exporter:latest
+	#docker push expressenab/bigip_exporter:$version
+	#docker push expressenab/bigip_exporter:latest
 }
 
-build_windows() {
-  echo "Building windows"
-  GOOS=windows GOARCH=amd64 go build .
-}
-
-package_windows() {
-  echo "Packaging windows"
-  version=$1
-  mkdir windows
-  mv bigip_exporter.exe windows/
-  cp LICENSE windows/
-  tar -zcvf bigip_exporter-$version.windows-amd64.tar.gz windows
-}
-
-clean_windows() {
-  echo "Cleaning windows"
-  rm -rf windows
-}
-
-build_linux() {
-  echo "Building linux"
-  GOOS=linux GOARCH=amd64 go build .
-}
-
-package_linux() {
-  echo "Packaging linux"
-  version=$1
-  mkdir linux
-  mv bigip_exporter linux/
-  cp LICENSE linux/
-  tar -zcvf bigip_exporter-$version.linux-amd64.tar.gz linux
-}
-
-clean_linux() {
-  echo "Cleaning linux"
-  rm -rf linux
-}
-
-
-fmt() {
-  find . -not -path "./vendor/*" -name "*.go" -exec go fmt {} \;
-}
-
-build_docker() {
-  version=$1
-  id=$(docker build . | awk 'END {print $3}')
-  if [[ $? != 0 ]]; then
-    echo "Docker build failed"
-    exit 1
-  fi
-  echo $id
-  docker tag $id expressenab/bigip_exporter:$version
-  docker tag $id expressenab/bigip_exporter:latest
-  docker push expressenab/bigip_exporter:$version
-  docker push expressenab/bigip_exporter:latest
-}
-
-if [[ $# == 0 ]]; then
-  echo "At least one argument is needed"
-elif [[ $1 == "fmt" ]]; then
-  fmt
+preflight
+if [[ $1 == "fmt" ]]; then
+	fmt
 else
-  build $1
-  package $1
-  clean
+	build 
 fi
