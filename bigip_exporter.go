e96e6c82825f30c4803d54b75d58e89cdbe71007
Tony Malkowski <tony@txstate.edu>
2024-12-17 21:50:00 -0600

    Add support for multiple sources

--- bigip_exporter.go
+++ bigip_exporter.go
@@ -2,55 +2,94 @@ package main
 
 import (
 	"net/http"
-	"strconv"
-	"strings"
+	"fmt"
 
 	"github.com/txst-sysops/prometheus-exporter-bigip/collector"
 	"github.com/txst-sysops/prometheus-exporter-bigip/config"
 	"github.com/juju/loggo"
 	"github.com/pr8kerl/f5er/f5"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-var (
-	logger = loggo.GetLogger("")
-)
+var logger = loggo.GetLogger("")
 
-func listen(exporterBindAddress string, exporterBindPort int) {
-	http.Handle("/metrics", prometheus.Handler())
+func configStringDefault(value string, fallback string) string {
+	if value != "" {
+		return value
+	}
+	return fallback
+}
+
+func configIntDefault(value int, fallback int) int {
+	if value != 0 {
+		return value
+	}
+	return fallback
+}
+
+func listen(exporterBindAddress string, exporterBindPort int, sources map[string]*prometheus.Registry) {
+	for sourceName, registry := range sources {
+		endpoint := fmt.Sprintf("/metrics/%s", sourceName)
+		http.Handle(endpoint, promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))
+		logger.Infof("Registered endpoint: %s", endpoint)
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`<html>
 			<head><title>BIG-IP Exporter</title></head>
 			<body>
 			<h1>BIG-IP Exporter</h1>
-			<p><a href="/metrics">Metrics</a></p>
+			<p>Use the following endpoints to scrape metrics:</p>
+			<ul>`))
+		for sourceName := range sources {
+			w.Write([]byte(fmt.Sprintf(`<li><a href="/metrics/%s">/metrics/%s</a></li>`, sourceName, sourceName)))
+		}
+		w.Write([]byte(`</ul>
 			</body>
 			</html>`))
 	})
-	exporterBind := exporterBindAddress + ":" + strconv.Itoa(exporterBindPort)
+
+	exporterBind := fmt.Sprintf("%s:%d", exporterBindAddress, exporterBindPort)
+	logger.Infof("Exporter listening on %s", exporterBind)
 	logger.Criticalf("Process failed: %s", http.ListenAndServe(exporterBind, nil))
 }
 
 func main() {
-	config := config.GetConfig()
-	logger.Debugf("Config: %v", config)
+	cfg := config.GetConfig()
 
-	bigipEndpoint := config.Bigip.Host + ":" + strconv.Itoa(config.Bigip.Port)
-	var exporterPartitionsList []string
-	if config.Exporter.Partitions != "" {
-		exporterPartitionsList = strings.Split(config.Exporter.Partitions, ",")
-	} else {
-		exporterPartitionsList = nil
+	// Map to hold separate registries for each source
+	sourceRegistries := make(map[string]*prometheus.Registry)
+
+	for name, source := range cfg.Sources {
+		cred, exists := cfg.Credentials[source.Credentials]
+		if !exists {
+			logger.Criticalf("Missing %s credentials for source %s", source.Credentials, name)
+			continue
+		}
+
+		sourcePort := configIntDefault(source.Port, 443)
+		bigipEndpoint := fmt.Sprintf("%s:%d", source.Host, sourcePort)
+		authMethod := f5.TOKEN
+		if cred.AuthType == "basic" {
+			authMethod = f5.BASIC_AUTH
+		}
+
+		bigip := f5.New(bigipEndpoint, cred.Username, cred.Password, authMethod)
+
+		bigipCollector, err := collector.NewBigipCollector(bigip, cfg.Exporter.Namespace, source.Partitions)
+		if err != nil {
+			logger.Criticalf("Failed to create collector for %s: %s", name, err)
+			continue
+		}
+
+		registry := prometheus.NewRegistry()
+		registry.MustRegister(bigipCollector)
+		sourceRegistries[name] = registry
 	}
-	authMethod := f5.TOKEN
-	if config.Bigip.BasicAuth {
-		authMethod = f5.BASIC_AUTH
-	}
 
-	bigip := f5.New(bigipEndpoint, config.Bigip.Username, config.Bigip.Password, authMethod)
-
-	bigipCollector, _ := collector.NewBigipCollector(bigip, config.Exporter.Namespace, exporterPartitionsList)
-
-	prometheus.MustRegister(bigipCollector)
-	listen(config.Exporter.BindAddress, config.Exporter.BindPort)
+	listenerHost := configStringDefault(cfg.Exporter.BindAddress, "0.0.0.0")
+	listenerPort := configIntDefault(cfg.Exporter.BindPort, 9142)
+	listen(listenerHost, listenerPort, sourceRegistries)
 }
+
--- config/config.go
+++ config/config.go
@@ -9,45 +9,49 @@ import (
 	"github.com/spf13/viper"
 )
 
-type bigipConfig struct {
-	Username  string `yaml:"username"`
-	Password  string `yaml:"password"`
-	BasicAuth bool   `yaml:"basic_auth"`
-	Host      string `yaml:"host"`
-	Port      int    `yaml:"port"`
+type Credential struct {
+	Username string `mapstructure:"username"`
+	Password string `mapstructure:"password"`
+	AuthType string `mapstructure:"authtype"`
 }
 
-type exporterConfig struct {
-	BindAddress string `yaml:"bind_address"`
-	BindPort    int    `yaml:"bind_port"`
-	Partitions  string `yaml:"partitions"`
-	Config      string `yaml:"config"`
-	Namespace   string `yaml:"namespace"`
-	LogLevel    string `yaml:"log_level"`
+type Source struct {
+	Host        string      `mapstructure:"host"`
+	Port        int         `mapstructure:"port"`
+	Credentials string      `mapstructure:"credentials"`
+	Partitions  []string    `mapstructure:"partitions"`
 }
 
-// Config is a container for settings modifiable by the user
+type ExporterConfig struct {
+	BindAddress string `mapstructure:"bind_address"`
+	BindPort    int    `mapstructure:"bind_port"`
+	LogLevel    string `mapstructure:"log_level"`
+	Namespace   string `mapstructure:"namespace"`
+}
+
 type Config struct {
-	Bigip    bigipConfig    `yaml:"bigip"`
-	Exporter exporterConfig `yaml:"exporter"`
+	Exporter    ExporterConfig        `mapstructure:"exporter"`
+	Credentials map[string]Credential `mapstructure:"credentials"`
+	Sources     map[string]Source     `mapstructure:"sources"`
 }
 
-var (
-	logger = loggo.GetLogger("")
-)
+var logger = loggo.GetLogger("")
 
 func init() {
 	loggo.ConfigureLoggers("<root>=INFO")
-	registerFlags()
-	bindFlags()
-	bindEnvs()
+
+	var configFile string
+	flag.StringVar(&configFile, "config", "config.yaml", "Path to config file")
 	flag.Parse()
 
-	if viper.GetString("exporter.config") != "" {
-		readConfigFile(viper.GetString("exporter.config"))
-	}
+	viper.SetConfigType("yaml")
+
+	viper.SetDefault("exporter.namespace", "bigip")
+
+	readConfigFile(configFile)
 
 	logLevel := viper.GetString("exporter.log_level")
+	logger.Infof("Using log level '%s'", logLevel)
 
 	if _, validLevel := loggo.ParseLevel(logLevel); validLevel {
 		loggo.ConfigureLoggers("<root>=" + strings.ToUpper(logLevel))
@@ -57,72 +61,28 @@ func init() {
 	logger.Warningf("Invalid log level - Using info")
 }
 
-func registerFlags() {
-	flag.Bool("bigip.basic_auth", false, "Use HTTP Basic authentication")
-	flag.String("bigip.host", "localhost", "The host on which f5 resides")
-	flag.Int("bigip.port", 443, "The port which f5 listens to")
-	flag.String("bigip.username", "user", "Username")
-	flag.String("bigip.password", "pass", "Password")
-	flag.String("exporter.bind_address", "localhost", "Exporter bind address")
-	flag.Int("exporter.bind_port", 9142, "Exporter bind port")
-	flag.String("exporter.partitions", "", "A comma separated list of partitions which to export. (default: all)")
-	flag.String("exporter.config", "", "bigip_exporter configuration file name.")
-	flag.String("exporter.namespace", "bigip", "bigip_exporter namespace.")
-	flag.String("exporter.log_level", "info", "Available options are trace, debug, info, warning, error and critical")
-}
-
-func bindFlags() {
-	flag.VisitAll(func(f *flag.Flag) {
-		err := viper.BindPFlag(f.Name, f)
-		if err != nil {
-			logger.Warningf("Failed to bind flag (%s)", err)
-		}
-	})
-}
-
-func bindEnvs() {
-	viper.SetEnvPrefix("be")
-	replacer := strings.NewReplacer(".", "_")
-	viper.SetEnvKeyReplacer(replacer)
-	flag.VisitAll(func(f *flag.Flag) {
-		err := viper.BindEnv(f.Name)
-		if err != nil {
-			logger.Warningf("Failed to bind environment variable BE_%s (%s)", strings.ToUpper(strings.Replace(f.Name, ".", "_", -1)), err)
-		}
-	})
-}
-
 func readConfigFile(fileName string) {
+	logger.Debugf("Reading config file %s", fileName)
 	file, err := os.Open(fileName)
 	if err != nil {
-		logger.Warningf("Failed to open configuration file (%s)", err)
+		logger.Criticalf("Failed to open configuration file: %s", err)
 		return
 	}
-	viper.SetConfigType("yaml")
-	err = viper.ReadConfig(file)
-	if err != nil {
-		logger.Warningf("Failed to read configuration file (%s)", err)
+	defer file.Close()
+	if err := viper.ReadConfig(file); err != nil {
+		logger.Criticalf("Failed to parse configuration file: %s", err)
+		return
 	}
+	logger.Infof("Successfully loaded configuration")
 }
 
 // GetConfig returns an instance of Config containing the resulting parameters
 // to the program
 func GetConfig() *Config {
-	return &Config{
-		Bigip: bigipConfig{
-			Username:  viper.GetString("bigip.username"),
-			Password:  viper.GetString("bigip.password"),
-			BasicAuth: viper.GetBool("bigip.basic_auth"),
-			Host:      viper.GetString("bigip.host"),
-			Port:      viper.GetInt("bigip.port"),
-		},
-		Exporter: exporterConfig{
-			BindAddress: viper.GetString("exporter.bind_address"),
-			BindPort:    viper.GetInt("exporter.bind_port"),
-			Partitions:  viper.GetString("exporter.partitions"),
-			Config:      viper.GetString("exporter.config"),
-			Namespace:   viper.GetString("exporter.namespace"),
-			LogLevel:    viper.GetString("exporter.log_level"),
-		},
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		logger.Criticalf("Failed to unmarshal config: %s", err)
+		os.Exit(1)
 	}
+	return &cfg
 }
